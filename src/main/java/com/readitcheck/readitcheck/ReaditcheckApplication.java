dfe693fbd82eef9c2be2d874842f98a9409b00d9
cbuzzard <corwin1998@gmail.com>
2020-08-04 17:05:23 -0400

    initial commit

---
+++ src/main/java/com/readitcheck/readitcheck/ReaditcheckApplication.java
@@ -0,0 +1,13 @@
+package com.readitcheck.readitcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaditcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaditcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readitcheck/readitcheck/ReaditcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readitcheck.readitcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaditcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
