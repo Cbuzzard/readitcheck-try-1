6a0b04f0a0d60428c096e544d341aabea9ff5600
cbuzzard <corwin1998@gmail.com>
2020-08-06 01:12:17 -0400

    Added id getter and setter

--- src/main/java/com/readitcheck/readitcheck/models/Commenter.java
+++ src/main/java/com/readitcheck/readitcheck/models/Commenter.java
@@ -24,6 +24,14 @@ public class Commenter {
 
     public Commenter() {}
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getUsername() {
         return username;
     }
--- src/main/java/com/readitcheck/readitcheck/models/Submission.java
+++ src/main/java/com/readitcheck/readitcheck/models/Submission.java
@@ -40,6 +40,14 @@ public class Submission {
 
     public Submission() {}
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
