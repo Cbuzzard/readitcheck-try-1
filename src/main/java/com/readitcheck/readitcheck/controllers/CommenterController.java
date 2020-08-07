5936fa061871380a7fe5ed65f1cdcd902266a47e
cbuzzard <corwin1998@gmail.com>
2020-08-07 17:45:43 -0400

    finished functionality to CommenterController

--- src/main/java/com/readitcheck/readitcheck/controllers/CommenterController.java
+++ src/main/java/com/readitcheck/readitcheck/controllers/CommenterController.java
@@ -1,4 +1,34 @@
 package com.readitcheck.readitcheck.controllers;
 
+import com.readitcheck.readitcheck.data.CommenterRepository;
+import com.readitcheck.readitcheck.models.Commenter;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController()
 public class CommenterController {
+
+    private final CommenterRepository repository;
+
+    public CommenterController(CommenterRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("commenter")
+    public List<Commenter> all(@RequestParam(required = false) Integer submissionId) {
+        return submissionId != null ? repository.findBySubmissionId(submissionId) : repository.findAll();
+    }
+
+    //TODO fix or else throw
+
+    @GetMapping("commenter/{id}")
+    public Commenter one(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @PostMapping("commenter")
+    public void newCommenter(@RequestBody Commenter commenter) {
+        repository.save(commenter);
+    }
+
 }
--- src/main/java/com/readitcheck/readitcheck/data/CommenterRepository.java
+++ src/main/java/com/readitcheck/readitcheck/data/CommenterRepository.java
@@ -4,6 +4,11 @@ import com.readitcheck.readitcheck.models.Commenter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommenterRepository extends JpaRepository<Commenter, Integer> {
+
+    List<Commenter> findBySubmissionId(Integer submissionId);
+
 }
