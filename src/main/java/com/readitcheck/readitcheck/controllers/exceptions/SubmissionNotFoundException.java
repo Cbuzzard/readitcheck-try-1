33622ab22c69588c195c216208acc4f3eb7660aa
cbuzzard <corwin1998@gmail.com>
2020-08-14 21:35:47 -0400

    Added exception for SubmissionNotFound

--- src/main/java/com/readitcheck/readitcheck/controllers/exceptions/SubmissionNotFoundException.java
+++ src/main/java/com/readitcheck/readitcheck/controllers/exceptions/SubmissionNotFoundException.java
@@ -1,4 +1,9 @@
 package com.readitcheck.readitcheck.controllers.exceptions;
 
 public class SubmissionNotFoundException extends RuntimeException{
+
+    SubmissionNotFoundException(Integer id) {
+        super("Could not find submission " + id);
+    }
+
 }
