b6396dc25f114fb40ee7befbf2accfb493869d90
cbuzzard <corwin1998@gmail.com>
2020-08-17 21:44:20 -0400

    improved error handling

--- src/main/java/com/readitcheck/readitcheck/controllers/exceptions/SubmissionNotFoundException.java
+++ src/main/java/com/readitcheck/readitcheck/controllers/exceptions/SubmissionNotFoundException.java
@@ -1,9 +1,19 @@
 package com.readitcheck.readitcheck.controllers.exceptions;
 
+
 public class SubmissionNotFoundException extends RuntimeException{
 
     public SubmissionNotFoundException(Integer id) {
         super("Could not find submission " + id);
+    }
+
+    public SubmissionNotFoundException(String title, String author) {
+        super("Could not find submission with title: " + title + ", author: " + author + ".");
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
     }
 
 }
